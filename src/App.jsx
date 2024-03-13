eb4507306942fcdddd8959cb04520e9086fb115a
María Delgado <mariad1996@gmail.com>
2024-03-13 19:15:04 +0100

    finished, revise before submitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import './App.css'
+import { useState } from 'react'
+import ReactDom from 'react-dom/client'
 
 function App() {
- //Aquí tu código
+    const [name, setName] = useState("Sofía")
+    const [newName, setNewName] = useState("")
+    const empty = document.getElementById('empty')
+
+    const changeName = (e) => {
+        e.preventDefault()
+        newName.trim() !== ""
+            ? (setName(newName), setNewName(''), empty.textContent = '')
+            : empty.textContent = 'Empty imput!'
+    }
+
+    return (
+        <div className="app-container">
+            <h2>Teacher name: {name}</h2>
+            <ul className="list">
+                <li onClick={() => setName('Data')}>Data</li>
+                <li onClick={() => setName('Reyes')}>Reyes</li>
+                <li onClick={() => setName('Yolanda')}>Yolanda</li>
+            </ul>
+
+            <div id="empty"></div>
+            <form onSubmit={changeName}>
+                <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="Add a name"></input>
+                <button type="submit">Add</button>
+            </form>
+
+            <button className="resetBtn" onClick={() => { setName('Sofía'); empty.textContent = '' }}>Reset</button>
+        </div>
+    )
 }
 
+ReactDom.createRoot(document.getElementById('root')).render(<App />)
 export default App
