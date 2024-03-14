ddf9b4cba4b7084aa56a438e7ca4077ffb48a265
María Delgado <mariad1996@gmail.com>
2024-03-14 11:12:25 +0100

    revised

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import './App.css'
 import { useState } from 'react'
-import ReactDom from 'react-dom/client'
 
 function App() {
-    const [name, setName] = useState("Sofía")
-    const [newName, setNewName] = useState("")
+    const [name, setName] = useState('Sofía')
+    const [newName, setNewName] = useState('')
     const empty = document.getElementById('empty')
 
     const changeName = (e) => {
         e.preventDefault()
-        newName.trim() !== ""
+        newName.trim() !== ''
             ? (setName(newName), setNewName(''), empty.textContent = '')
             : empty.textContent = 'Empty imput!'
     }
 
+    const resetApp = () => {
+        setName('Sofía'); 
+        setNewName('');
+        empty.textContent = ''
+    }
+
     return (
-        <div className="app-container">
+        <>
             <h2>Teacher name: {name}</h2>
             <ul className="list">
                 <li onClick={() => setName('Data')}>Data</li>
@@ -23,16 +28,20 @@ function App() {
                 <li onClick={() => setName('Yolanda')}>Yolanda</li>
             </ul>
 
-            <div id="empty"></div>
             <form onSubmit={changeName}>
                 <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="Add a name"></input>
                 <button type="submit">Add</button>
             </form>
 
-            <button className="resetBtn" onClick={() => { setName('Sofía'); empty.textContent = '' }}>Reset</button>
-        </div>
+            <div id="empty"></div>
+            <button className="resetBtn" onClick={resetApp}>Reset app</button>
+        </>
     )
 }
 
-ReactDom.createRoot(document.getElementById('root')).render(<App />)
 export default App
+
+// He comentado estas líneas porque en la consola del navegador salía un error que decía que estaba usando createRoot() 2 veces sobre el mismo elemento. He dejado solo el de main.jsx funcionando para corregirlo.
+
+// import ReactDom from 'react-dom/client' // No me hace falta porque no lo uso aquí
+// ReactDom.createRoot(document.getElementById('root')).render(<App />) // Línea que causaba el error
